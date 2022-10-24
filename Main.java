0a00d733ac44c3de590e1efafa58458f027528df
David Griswold <david.griswold@graded.br>
2022-10-24 11:10:07 -0300

    initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+// this template can be used for any Turtle questions in CS Awesome
+
+import java.awt.*;
+
+public class Main {
+  public static void main(String[] args) {  
+    World world = new World(400,400);
+    Turtle yertle = new Turtle(world);
+    // Add your own code in here 
+    yertle.setColor(Color.green);
+    yertle.forward();
+    yertle.turnRight();
+    yertle.forward();
+    
+    world.setVisible(true);
+  }
+}
