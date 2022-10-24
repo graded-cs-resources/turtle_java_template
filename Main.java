7fae88682aab72f66275ae9fbbd3c9c8a3ce2a1c
David Griswold <david.griswold@graded.br>
2022-10-24 15:00:51 +0000

    fully updated

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class Main {
   public static void main(String[] args) {  
-    World world = new World(400,400);
+    World world = new World(640,400);
     Turtle yertle = new Turtle(world);
     // Add your own code in here 
     yertle.setColor(Color.green);
